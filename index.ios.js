ec4340c08ec87ca00b67593e21d0799d5ac6ce74
bin <bin@sunbindeMacBook-Pro.local>
2017-05-17 19:40:58 +0800

    init

---
+++ index.ios.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class rn_pro extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{width: 350,height:200}}
+          source={require('./static/file.jpg')}
+        />
+        <Text style={styles.welcome}>
+         native test!!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('rn_pro', () => rn_pro);
